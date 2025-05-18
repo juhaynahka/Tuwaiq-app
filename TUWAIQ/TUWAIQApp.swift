300a982148394dbb15f69f81f16dc4f92cf409fa
juhaynah <juhaynahaljuhane@gmail.com>
2025-05-18 11:12:57 +0300

    Initial Commit

---
+++ TUWAIQ/TUWAIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  TUWAIQApp.swift
+//  TUWAIQ
+//
+//  Created by Tuwaiq.IT on 20/11/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TUWAIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
